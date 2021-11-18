806226e56112d6b5d7e53a483c487dc059aef3bf
indrasupendisinaga <indrasupendisinaga@gmail.com>
2021-11-18 15:32:24 +0700

    menambahkan sourcecode

--- perulangan-latihan2.py
+++ perulangan-latihan2.py
@@ -3,3 +3,10 @@ print ("Pertemuan ke 7")
 print ("kondisional-perulangan")
 print ("perulangan-latihan 1")
 print ("------------------")
+
+x=int   (input("masukkkan jumlah baris:")
+
+for i in range("*"):
+for j in range (1):
+        print("*", end=" ")
+    print()
