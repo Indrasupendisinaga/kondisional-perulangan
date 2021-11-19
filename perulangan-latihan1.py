424f2215b53892c4345ff64eaf5de2dfb32eb847
indrasupendisinaga <indrasupendisinaga@gmail.com>
2021-11-19 10:09:33 +0700

    penambahan foto

--- perulangan-latihan1.py
+++ perulangan-latihan1.py
@@ -2,25 +2,3 @@ print ("------------------")
 print ("Pertemuan ke 7")
 print ("kondisional-perulangan")
 print ("perulangan-latihan 1")
-print ("Buat program sederhana dengan input 2 buah bilangan, kemudian")
-print ("tentukan bilangan terbesar dari kedua bilangan tersebut")
-print ("menggunakan statement if")
-print ("------------------")
-
-def main():
-    
-    # menentukan input user
-    a = int(input("masukkan bilangan pertama: "))
-    b = int(input("masukkan bilangan kedua: "))
- 
-     # Menentukan Nilai Bilangan  dengan if else
-    if a > b:
-        maks = a
-    else:
-        maks = b
-    # mencetak nilai maksimum
-    print ("-------------")
-    print('Nilai Terbesar adalah %d' % maks)
-
-if __name__ == '__main__':
-    main()
--- perulangan-latihan2.py
+++ perulangan-latihan2.py
@@ -4,7 +4,7 @@ print ("kondisional-perulangan")
 print ("perulangan-latihan 1")
 print ("------------------")
 
-x=int   (input("masukkkan jumlah baris:")
+x=int   (input("masukkan jumlah baris:")
 
 for i in range("*"):
 for j in range (1):
